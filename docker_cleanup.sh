2c039bf7969c7bee1c4822ef558179f3dd2bcc08
orgaworl <64845428+orgaworl@users.noreply.github.com>
2025-02-21 20:48:30 +0800

    Remove unnecessary components.

--- docker_cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do
-	eval $(minikube docker-env) && docker container prune --force
-	sleep 3600
-done
--- k8/cleanlogs.sh
+++
@@ -1,7 +0,0 @@
-echo "cleaning all folders..."
-rm -rf done/*
-rm -rf retries/*
-rm -f logs/*
-rm -rf patches/*
-rm -rf results/*
-echo "done"
